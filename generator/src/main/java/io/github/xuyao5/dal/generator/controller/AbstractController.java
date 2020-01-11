c03c7411e83f4bcf2b3496ddd00565d582d59c85
Thomas.XU <xuyao5@gmail.com>
2020-01-11 22:38:53 +0800

    modify controller

--- generator/src/main/java/io/github/xuyao5/dal/generator/controller/AbstractController.java
+++ generator/src/main/java/io/github/xuyao5/dal/generator/controller/AbstractController.java
@@ -1,8 +1,5 @@
 package io.github.xuyao5.dal.generator.controller;
 
-import lombok.extern.log4j.Log4j2;
-
-
 public abstract class AbstractController {
 
 }
--- generator/src/main/java/io/github/xuyao5/dal/generator/controller/generator/GeneratorController.java
+++ generator/src/main/java/io/github/xuyao5/dal/generator/controller/generator/GeneratorController.java
@@ -3,6 +3,7 @@ package io.github.xuyao5.dal.generator.controller.generator;
 import io.github.xuyao5.dal.generator.controller.AbstractController;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 @RequestMapping(path = "/generator")
 public class GeneratorController extends AbstractController {
 
+    @PostMapping(path = "/generateMyBatisFiles")
     public void generateMyBatisFiles(@NotNull GenerateMyBatisFilesRequest request) {
 
     }
