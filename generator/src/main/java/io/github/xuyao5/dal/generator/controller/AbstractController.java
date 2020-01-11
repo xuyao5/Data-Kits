55c03b3159103735abda99b9bc2d8b7539341360
Thomas.XU <xuyao5@gmail.com>
2020-01-11 22:34:01 +0800

    delete web & add controller

---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/controller/AbstractController.java
@@ -0,0 +1,8 @@
+package io.github.xuyao5.dal.generator.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+
+public abstract class AbstractController {
+
+}
---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/controller/generator/GeneratorController.java
@@ -0,0 +1,24 @@
+package io.github.xuyao5.dal.generator.controller.generator;
+
+import io.github.xuyao5.dal.generator.controller.AbstractController;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+@Log4j2
+@RestController
+@RequestMapping(path = "/generator")
+public class GeneratorController extends AbstractController {
+
+    public void generateMyBatisFiles(@NotNull GenerateMyBatisFilesRequest request) {
+
+    }
+
+    @Data(staticConstructor = "of")
+    class GenerateMyBatisFilesRequest {
+
+    }
+}
