575eb0301ce0a35f8578fc44f76ada05c734f8be
Thomas.XU <xuyao5@gmail.com>
2020-01-11 22:01:46 +0800

    add MyBatisInitializr

---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/consts/SupportDatabase.java
@@ -0,0 +1,28 @@
+package io.github.xuyao5.dal.generator.consts;
+
+import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@AllArgsConstructor
+public enum SupportDatabase {
+
+    ORACLE("ORACLE","Oracle"),
+    MYSQL("MYSQL","MySql"),
+    ;
+
+    @Getter
+    private final String type;
+
+    @Getter
+    private final String description;
+
+    public static Optional<SupportDatabase> getSupportDatabaseByType(@NotNull String type){
+       return Arrays.stream(SupportDatabase.values()).parallel()
+                .filter(supportDatabase -> supportDatabase.getType().equalsIgnoreCase(type))
+                .findAny();
+    }
+}
