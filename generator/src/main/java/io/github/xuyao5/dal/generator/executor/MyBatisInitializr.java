80f9612c4c05fcabf37a29329ee88454dcbf1857
Thomas.XU <xuyao5@gmail.com>
2020-01-12 22:36:02 +0800

    update MyBatisInitializr

--- generator/src/main/java/io/github/xuyao5/dal/generator/executor/MyBatisInitializr.java
+++ generator/src/main/java/io/github/xuyao5/dal/generator/executor/MyBatisInitializr.java
@@ -1,5 +1,6 @@
 package io.github.xuyao5.dal.generator.executor;
 
+import lombok.extern.log4j.Log4j2;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -10,19 +11,24 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+@Log4j2
 public class MyBatisInitializr {
 
-    public void initial() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
-        List<String> warnings = new ArrayList<>();
+    public void gen() {
+        List<String> warnings = new CopyOnWriteArrayList<>();
         boolean overwrite = true;
         File configFile = new File("generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(configFile);
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        myBatisGenerator.generate(null);
+        try {
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (IOException | InterruptedException | SQLException | InvalidConfigurationException | XMLParserException ex) {
+            log.error(ex);
+        }
     }
 }
