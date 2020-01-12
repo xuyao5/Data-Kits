848ed65533d322d0ff041a7dcdeddc8b3c857d6e
Thomas.XU <xuyao5@gmail.com>
2020-01-12 22:46:48 +0800

    add GeneratorPropertyBean

---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/configuration/GeneratorConfiguration.java
@@ -0,0 +1,18 @@
+package io.github.xuyao5.dal.generator.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({})
+@ComponentScan(basePackages = {"io.github.xuyao5.dal.generator"})
+public class GeneratorConfiguration {
+
+    @Bean
+    public GeneratorPropertyBean createPropertyBean() {
+        return GeneratorPropertyBean.of();
+    }
+
+}
---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/configuration/GeneratorPropertyBean.java
@@ -0,0 +1,10 @@
+package io.github.xuyao5.dal.generator.configuration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(staticName = "of")
+public final class GeneratorPropertyBean {
+
+}
