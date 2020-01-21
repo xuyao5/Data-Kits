47c697b62efd72fd7a1e6f64849864c346d33b9e
Thomas.XU <xuyao5@gmail.com>
2020-01-21 22:48:11 +0800

    add example mapper files for test

---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/repository/paces/dao/BatchJobExecutionDynamicSqlSupport.java
@@ -0,0 +1,74 @@
+package io.github.xuyao5.dal.generator.repository.paces.dao;
+
+import java.sql.JDBCType;
+import java.util.Date;
+import javax.annotation.Generated;
+import org.mybatis.dynamic.sql.SqlColumn;
+import org.mybatis.dynamic.sql.SqlTable;
+
+public final class BatchJobExecutionDynamicSqlSupport {
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final BatchJobExecution batchJobExecution = new BatchJobExecution();
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> jobExecutionId = batchJobExecution.jobExecutionId;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> version = batchJobExecution.version;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Long> jobInstanceId = batchJobExecution.jobInstanceId;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Date> createTime = batchJobExecution.createTime;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Date> startTime = batchJobExecution.startTime;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Date> endTime = batchJobExecution.endTime;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> status = batchJobExecution.status;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> exitCode = batchJobExecution.exitCode;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> exitMessage = batchJobExecution.exitMessage;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<Date> lastUpdated = batchJobExecution.lastUpdated;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final SqlColumn<String> jobConfigurationLocation = batchJobExecution.jobConfigurationLocation;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public static final class BatchJobExecution extends SqlTable {
+        public final SqlColumn<Long> jobExecutionId = column("JOB_EXECUTION_ID", JDBCType.BIGINT);
+
+        public final SqlColumn<Long> version = column("VERSION", JDBCType.BIGINT);
+
+        public final SqlColumn<Long> jobInstanceId = column("JOB_INSTANCE_ID", JDBCType.BIGINT);
+
+        public final SqlColumn<Date> createTime = column("CREATE_TIME", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<Date> startTime = column("START_TIME", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<Date> endTime = column("END_TIME", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<String> status = column("\"STATUS\"", JDBCType.VARCHAR);
+
+        public final SqlColumn<String> exitCode = column("EXIT_CODE", JDBCType.VARCHAR);
+
+        public final SqlColumn<String> exitMessage = column("EXIT_MESSAGE", JDBCType.VARCHAR);
+
+        public final SqlColumn<Date> lastUpdated = column("LAST_UPDATED", JDBCType.TIMESTAMP);
+
+        public final SqlColumn<String> jobConfigurationLocation = column("JOB_CONFIGURATION_LOCATION", JDBCType.VARCHAR);
+
+        public BatchJobExecution() {
+            super("BATCH_JOB_EXECUTION");
+        }
+    }
+}
---
+++ generator/src/main/java/io/github/xuyao5/dal/generator/repository/paces/model/BatchJobInstance.java
@@ -0,0 +1,132 @@
+package io.github.xuyao5.dal.generator.repository.paces.model;
+
+import java.io.Serializable;
+import javax.annotation.Generated;
+
+public class BatchJobInstance implements Serializable {
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    private Long jobInstanceId;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    private Long version;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    private String jobName;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    private String jobKey;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    private static final long serialVersionUID = 1L;
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public Long getJobInstanceId() {
+        return jobInstanceId;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public BatchJobInstance withJobInstanceId(Long jobInstanceId) {
+        this.setJobInstanceId(jobInstanceId);
+        return this;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public void setJobInstanceId(Long jobInstanceId) {
+        this.jobInstanceId = jobInstanceId;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public Long getVersion() {
+        return version;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public BatchJobInstance withVersion(Long version) {
+        this.setVersion(version);
+        return this;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public String getJobName() {
+        return jobName;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public BatchJobInstance withJobName(String jobName) {
+        this.setJobName(jobName);
+        return this;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public void setJobName(String jobName) {
+        this.jobName = jobName == null ? null : jobName.trim();
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public String getJobKey() {
+        return jobKey;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public BatchJobInstance withJobKey(String jobKey) {
+        this.setJobKey(jobKey);
+        return this;
+    }
+
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public void setJobKey(String jobKey) {
+        this.jobKey = jobKey == null ? null : jobKey.trim();
+    }
+
+    @Override
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        BatchJobInstance other = (BatchJobInstance) that;
+        return (this.getJobInstanceId() == null ? other.getJobInstanceId() == null : this.getJobInstanceId().equals(other.getJobInstanceId()))
+            && (this.getVersion() == null ? other.getVersion() == null : this.getVersion().equals(other.getVersion()))
+            && (this.getJobName() == null ? other.getJobName() == null : this.getJobName().equals(other.getJobName()))
+            && (this.getJobKey() == null ? other.getJobKey() == null : this.getJobKey().equals(other.getJobKey()));
+    }
+
+    @Override
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getJobInstanceId() == null) ? 0 : getJobInstanceId().hashCode());
+        result = prime * result + ((getVersion() == null) ? 0 : getVersion().hashCode());
+        result = prime * result + ((getJobName() == null) ? 0 : getJobName().hashCode());
+        result = prime * result + ((getJobKey() == null) ? 0 : getJobKey().hashCode());
+        return result;
+    }
+
+    @Override
+    @Generated("org.mybatis.generator.api.MyBatisGenerator")
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", jobInstanceId=").append(jobInstanceId);
+        sb.append(", version=").append(version);
+        sb.append(", jobName=").append(jobName);
+        sb.append(", jobKey=").append(jobKey);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
