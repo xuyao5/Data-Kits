fb60716ed1d08b622651c2d1e88d2169eba9cb84
Thomas.XU <xuyao5@gmail.com>
2020-04-05 13:49:52 +0800

    add ProxyRedisCachingConfiguration

---
+++ RedisKits/src/main/java/io/github/xuyao5/dal/redis/configuration/ProxyRedisCachingConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.xuyao5.dal.redis.configuration;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+
+@Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class ProxyRedisCachingConfiguration {//extends AbstractCachingConfiguration
+
+}
--- RedisKits/src/main/java/io/github/xuyao5/dal/redis/configuration/selector/RedisCachingConfigurationSelector.java
+++ RedisKits/src/main/java/io/github/xuyao5/dal/redis/configuration/selector/RedisCachingConfigurationSelector.java
@@ -1,21 +1,33 @@
 package io.github.xuyao5.dal.redis.configuration.selector;
 
 import io.github.xuyao5.dal.redis.annotation.EnableRedisCaching;
+import io.github.xuyao5.dal.redis.configuration.ProxyRedisCachingConfiguration;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.AdviceModeImportSelector;
+import org.springframework.context.annotation.AutoProxyRegistrar;
 
 public class RedisCachingConfigurationSelector extends AdviceModeImportSelector<EnableRedisCaching> {
+
+    private static final String CACHE_ASPECT_CONFIGURATION_CLASS_NAME = "org.springframework.cache.aspectj.AspectJCachingConfiguration";
 
     @Override
     protected String[] selectImports(AdviceMode adviceMode) {
         System.out.println("这是RedisCachingConfigurationSelector，哈哈哈");
         switch (adviceMode) {
             case PROXY:
-                return new String[0];
+                return getProxyImports();
             case ASPECTJ:
-                return new String[1];
+                return getAspectJImports();
             default:
                 return null;
         }
+    }
+
+    private String[] getProxyImports() {
+        return new String[]{AutoProxyRegistrar.class.getName(), ProxyRedisCachingConfiguration.class.getName()};
+    }
+
+    private String[] getAspectJImports() {
+        return new String[]{CACHE_ASPECT_CONFIGURATION_CLASS_NAME};
     }
 }
