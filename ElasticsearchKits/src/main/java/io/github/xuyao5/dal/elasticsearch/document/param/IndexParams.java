ee6524b80060e2a9defe54732d10eb30e11021dc
Thomas.XU <xuyao5@gmail.com>
2020-05-05 14:32:05 +0800

    update EsDocumentSupporter

---
+++ ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/document/param/IndexParams.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.dal.elasticsearch.document.param;
+
+import lombok.Data;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 5/05/20 14:25
+ * @apiNote IndexParams
+ * @implNote IndexParams
+ */
+@Data(staticConstructor = "of")
+public final class IndexParams {
+}
---
+++ ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/document/param/ReindexParams.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.dal.elasticsearch.document.param;
+
+import lombok.Data;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 5/05/20 14:27
+ * @apiNote ReindexParams
+ * @implNote ReindexParams
+ */
+@Data(staticConstructor = "of")
+public final class ReindexParams {
+}
