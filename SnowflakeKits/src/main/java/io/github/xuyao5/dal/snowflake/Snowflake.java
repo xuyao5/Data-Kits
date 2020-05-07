1ea44a5a78d0d07ceaee59970e5f3713800c91ca
Thomas.XU <xuyao5@gmail.com>
2020-05-07 21:07:11 +0800

    add SnowflakeKits module

---
+++ SnowflakeKits/src/main/java/io/github/xuyao5/dal/snowflake/Snowflake.java
@@ -0,0 +1,15 @@
+package io.github.xuyao5.dal.snowflake;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 7/05/20 21:02
+ * @apiNote Snowflake
+ * @implNote Snowflake
+ */
+@Slf4j
+@Component("snowflake")
+public final class Snowflake {
+}
---
+++ SnowflakeKits/src/main/java/io/github/xuyao5/dal/snowflake/configuration/SnowflakeKitsConfigBean.java
@@ -0,0 +1,15 @@
+package io.github.xuyao5.dal.snowflake.configuration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 7/05/20 21:02
+ * @apiNote SnowflakeKitsConfigBean
+ * @implNote SnowflakeKitsConfigBean
+ */
+@Getter
+@RequiredArgsConstructor(staticName = "of")
+public final class SnowflakeKitsConfigBean {
+}
