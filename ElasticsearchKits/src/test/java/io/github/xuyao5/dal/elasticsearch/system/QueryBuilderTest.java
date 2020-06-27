1682b728a4544af77cb39be7809f507a9f75ef63
Thomas.XU <xuyao5@gmail.com>
2020-06-27 22:36:58 +0800

    add order script

--- ElasticsearchKits/src/test/java/io/github/xuyao5/dal/elasticsearch/system/QueryBuilderTest.java
+++ ElasticsearchKits/src/test/java/io/github/xuyao5/dal/elasticsearch/system/QueryBuilderTest.java
@@ -6,6 +6,10 @@ import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.search.sort.ScriptSortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 
 public class QueryBuilderTest extends AbstractTest {
@@ -13,6 +17,8 @@ public class QueryBuilderTest extends AbstractTest {
     @SneakyThrows
     @Test
     void testQueryBuilder() {
+        ScriptSortBuilder scriptSortBuilder = SortBuilders.scriptSort(new Script("Math.random()"), ScriptSortBuilder.ScriptSortType.NUMBER).order(SortOrder.DESC);
+        System.out.println(scriptSortBuilder);
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         boolQueryBuilder.filter(QueryBuilders.prefixQuery("scc", "11"));
         boolQueryBuilder.filter(QueryBuilders.prefixQuery("scc", "92"));
@@ -20,7 +26,8 @@ public class QueryBuilderTest extends AbstractTest {
         boolQueryBuilder.filter(QueryBuilders.prefixQuery("scc", "23"));
         boolQueryBuilder.filter(QueryBuilders.rangeQuery("age").from(20).to(65));
         boolQueryBuilder.filter(QueryBuilders.termsQuery("org", "1", "2"));
-        NestedQueryBuilder myPath = QueryBuilders.nestedQuery("myPath", boolQueryBuilder, ScoreMode.None);
+        NestedQueryBuilder myPath = QueryBuilders
+                .nestedQuery("myPath", boolQueryBuilder, ScoreMode.None);
         System.out.println(myPath);
     }
 }
