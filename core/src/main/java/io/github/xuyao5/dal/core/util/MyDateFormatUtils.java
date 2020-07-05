d8208e5680ca0b873506c158ae4efb0574eb5be7
Thomas.XU <xuyao5@gmail.com>
2020-07-05 17:23:30 +0800

    工具类占位

---
+++ core/src/main/java/io/github/xuyao5/dal/core/util/MyDateFormatUtils.java
@@ -0,0 +1,34 @@
+package io.github.xuyao5.dal.core.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 5/07/20 17:10
+ * @apiNote MyDateFormatUtils
+ * @implNote MyDateFormatUtils
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MyDateFormatUtils extends DateFormatUtils {
+
+    public static String convertDate2String(@NotNull String date) {
+        return null;
+    }
+
+    public static String convertDate2String(@NotNull Date date) {
+        return null;
+    }
+
+    public static Date convertDate2Date(@NotNull String date) {
+        return null;
+    }
+
+    public static Date convertDate2Date(@NotNull Date date) {
+        return null;
+    }
+}
---
+++ core/src/main/java/io/github/xuyao5/dal/core/util/MyStringUtils.java
@@ -0,0 +1,17 @@
+package io.github.xuyao5.dal.core.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 5/07/20 17:19
+ * @apiNote MyStringUtils
+ * @implNote MyStringUtils
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MyStringUtils extends StringUtils {
+
+}
