1dbb27779b8ac66945d0063be5bb6a9ab8eeec78
Thomas.XU <xuyao5@gmail.com>
2020-09-26 22:23:04 +0800

    new disruptor demo

---
+++ file2es/src/test/java/io/github/xuyao5/dal/file2es/disruptor/DisruptorTest.java
@@ -0,0 +1,44 @@
+package io.github.xuyao5.dal.file2es.disruptor;
+
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 26/09/20 22:06
+ * @apiNote TODO 这里输入文件说明
+ * @implNote TODO 这里输入实现说明
+ */
+public class DisruptorTest {
+
+    @SneakyThrows
+    @Test
+    void test() {
+        // Specify the size of the ring buffer, must be power of 2.
+        int bufferSize = 1024;
+
+        // Construct the Disruptor
+        Disruptor<LongEvent> disruptor = new Disruptor<>(new LongEventFactory(), bufferSize, DaemonThreadFactory.INSTANCE);
+
+        // Connect the handler
+        disruptor.handleEventsWith((event, sequence, endOfBatch) -> System.out.println("Event: " + event));
+
+        // Start the Disruptor, starts all threads running
+        disruptor.start();
+
+        // Get the ring buffer from the Disruptor to be used for publishing.
+        RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+
+        ByteBuffer bb = ByteBuffer.allocate(8);
+        for (long l = 0; true; l++) {
+            bb.putLong(0, l);
+            ringBuffer.publishEvent((event, sequence, buffer) -> event.setValue(buffer.getLong(0)), bb);
+            Thread.sleep(1000);
+        }
+    }
+}
