95fe85b6d3cf14788ea90f67325a5a07270f1d83
Thomas.XU <xuyao5@gmail.com>
2020-10-14 21:50:36 +0800

    配置WebSocketMessageBroker

--- ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/configuration/ElasticsearchKitsConfiguration.java
+++ ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/configuration/ElasticsearchKitsConfiguration.java
@@ -2,12 +2,28 @@ package io.github.xuyao5.dal.elasticsearch.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-public class ElasticsearchKitsConfiguration {
+@EnableWebSocketMessageBroker
+public class ElasticsearchKitsConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Bean
     public ElasticsearchKitsConfigBean elasticsearchKitsConfigBean() {
         return ElasticsearchKitsConfigBean.of();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
 }
