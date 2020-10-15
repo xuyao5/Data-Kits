85a6a57ddb625102cb34d564eb4282c586c44934
Thomas.XU <xuyao5@gmail.com>
2020-10-15 23:56:09 +0800

    注释部分代码

--- ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/controller/QueryController.java
+++ ElasticsearchKits/src/main/java/io/github/xuyao5/dal/elasticsearch/controller/QueryController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
  * @implNote TODO 这里输入实现说明
  */
 @RestController
-@MessageMapping("query")
+//@MessageMapping("query")
 public final class QueryController extends AbstractController {
 
     @GetMapping("/hello")
