68883e571408fe1700b7eff01cf93207d65d68e8
Thomas.XU <xuyao5@gmail.com>
2020-11-17 00:00:46 +0800

    删除RedisKitsServer

--- RedisKitsServer/src/test/java/io/github/xuyao5/dal/redis/abstr/AbstractTest.java
+++
@@ -1,8 +0,0 @@
-package io.github.xuyao5.dal.redis.abstr;
-
-import io.github.xuyao5.dal.redis.RedisKitsApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = {RedisKitsApplication.class})
-public abstract class AbstractTest {
-}
