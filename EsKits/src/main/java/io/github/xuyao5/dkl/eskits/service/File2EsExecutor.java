247130d0c53abd83607db796c290b659eebb769c
Thomas.XU <xuyao5@gmail.com>
2020-12-29 17:42:25 +0800

    bug fix

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/service/File2EsExecutor.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/service/File2EsExecutor.java
@@ -25,7 +25,7 @@ public final class File2EsExecutor {
 
     @SneakyThrows
     public void execute(@NotNull File2EsTask task) {
-        List<File> decisionFiles = MyFileUtils.getDecisionFiles(task.getFilePath(), task.getFilePath());
+        List<File> decisionFiles = MyFileUtils.getDecisionFiles(task.getFilePath(), task.getFilePattern());
         decisionFiles.stream()
                 .filter(file -> Files.exists(Paths.get(MyFilenameUtils.getConfirmFilename(file.getPath(), task.getFileNameSeparator(), task.getFileConfirmPrefix(), task.getFileConfirmSuffix()))))
                 .forEach(file -> {
