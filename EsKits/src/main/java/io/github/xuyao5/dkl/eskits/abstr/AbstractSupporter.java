babb1d02d7f7bb9fbf7355c51e359c045d300c1b
Thomas.XU <xuyao5@gmail.com>
2021-01-15 22:44:55 +0800

    去JavaPoet

---
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/abstr/AbstractSupporter.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.dkl.eskits.abstr;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 12/01/21 18:28
+ * @apiNote TODO 这里输入文件说明
+ * @implNote TODO 这里输入实现说明
+ */
+@Slf4j
+public class AbstractSupporter {
+}
