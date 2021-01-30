aba7fe2f53bd1e69906b8b25c4838be479247e60
Thomas.XU <xuyao5@gmail.com>
2021-01-30 19:43:57 +0800

    fix something

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/abstr/AbstractSupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/abstr/AbstractSupporter.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 12/01/21 18:28
- * @apiNote TODO 这里输入文件说明
- * @implNote TODO 这里输入实现说明
+ * @apiNote AbstractSupporter
+ * @implNote AbstractSupporter
  */
 @Slf4j
-public class AbstractSupporter {
+public abstract class AbstractSupporter {
 }
