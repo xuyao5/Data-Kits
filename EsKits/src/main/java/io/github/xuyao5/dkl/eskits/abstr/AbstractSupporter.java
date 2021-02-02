892a13b251d2bbb23906972914e30b75fa8fd52c
Thomas.XU <xuyao5@gmail.com>
2021-02-02 21:20:33 +0800

    更新AbstractSupporter

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/abstr/AbstractSupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/abstr/AbstractSupporter.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 12/01/21 18:28
@@ -15,5 +17,18 @@ import org.elasticsearch.client.RestHighLevelClient;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractSupporter {
 
+    protected static final int DEFAULT_SHARDS_SLICES = 10;
+    protected static final int DEFAULT_SCROLL_MINUTES = 10;
+
     protected final RestHighLevelClient restHighLevelClient;
+
+    /**
+     * 记录执行时间的标准化日志
+     *
+     * @param method          执行方法
+     * @param startTimeMillis 开始时间
+     */
+    protected void logElapsed(@NotNull String method, long startTimeMillis) {
+        log.info("[{}]{}ms", method, System.currentTimeMillis() - startTimeMillis);
+    }
 }
