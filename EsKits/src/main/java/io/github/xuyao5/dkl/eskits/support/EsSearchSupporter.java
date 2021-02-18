8c6cc7e402e4017642584c567b37ab5747b0be29
Thomas.XU <xuyao5@gmail.com>
2021-02-18 12:41:05 +0800

    清理代码

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/EsSearchSupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/EsSearchSupporter.java
@@ -2,17 +2,12 @@ package io.github.xuyao5.dkl.eskits.support;
 
 import io.github.xuyao5.dkl.eskits.abstr.AbstractSupporter;
 import lombok.SneakyThrows;
-import org.elasticsearch.action.explain.ExplainRequest;
-import org.elasticsearch.action.explain.ExplainResponse;
-import org.elasticsearch.action.fieldcaps.FieldCapabilitiesRequest;
-import org.elasticsearch.action.fieldcaps.FieldCapabilitiesResponse;
 import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.core.CountRequest;
 import org.elasticsearch.client.core.CountResponse;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.rankeval.*;
 import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.script.mustache.MultiSearchTemplateRequest;
 import org.elasticsearch.script.mustache.MultiSearchTemplateResponse;
@@ -21,7 +16,8 @@ import org.elasticsearch.script.mustache.SearchTemplateResponse;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import javax.validation.constraints.NotNull;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
 
@@ -75,7 +71,7 @@ public final class EsSearchSupporter extends AbstractSupporter {
      * Multi-Search API
      */
     @SneakyThrows
-    public MultiSearchResponse msearch() {
+    public MultiSearchResponse multiSearch() {
         MultiSearchRequest request = new MultiSearchRequest();
         SearchRequest firstSearchRequest = new SearchRequest();
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
@@ -104,10 +100,10 @@ public final class EsSearchSupporter extends AbstractSupporter {
     }
 
     /**
-     * Multi-Search-Template API
+     * Multi Search Template API
      */
     @SneakyThrows
-    public MultiSearchTemplateResponse msearchTemplate() {
+    public MultiSearchTemplateResponse multiSearchTemplate() {
         String[] searchTerms = {"elasticsearch", "logstash", "kibana"};
 
         MultiSearchTemplateRequest multiRequest = new MultiSearchTemplateRequest();
@@ -131,47 +127,6 @@ public final class EsSearchSupporter extends AbstractSupporter {
             multiRequest.add(request);
         }
         return client.msearchTemplate(multiRequest, DEFAULT);
-    }
-
-    /**
-     * Field Capabilities API
-     */
-    @SneakyThrows
-    public FieldCapabilitiesResponse fieldCaps() {
-        FieldCapabilitiesRequest request = new FieldCapabilitiesRequest()
-                .fields("user")
-                .indices("posts", "authors", "contributors");
-        return client.fieldCaps(request, DEFAULT);
-    }
-
-    /**
-     * Ranking Evaluation API
-     */
-    @SneakyThrows
-    public RankEvalResponse rankEval() {
-        EvaluationMetric metric = new PrecisionAtK();
-        List<RatedDocument> ratedDocs = new ArrayList<>();
-        ratedDocs.add(new RatedDocument("posts", "1", 1));
-        SearchSourceBuilder searchQuery = new SearchSourceBuilder();
-        searchQuery.query(QueryBuilders.matchQuery("user", "kimchy"));
-        RatedRequest ratedRequest =
-                new RatedRequest("kimchy_query", ratedDocs, searchQuery);
-        List<RatedRequest> ratedRequests = Arrays.asList(ratedRequest);
-        RankEvalSpec specification =
-                new RankEvalSpec(ratedRequests, metric);
-        RankEvalRequest request =
-                new RankEvalRequest(specification, new String[]{"posts"});
-        return client.rankEval(request, DEFAULT);
-    }
-
-    /**
-     * Explain API
-     */
-    @SneakyThrows
-    public ExplainResponse explain() {
-        ExplainRequest request = new ExplainRequest("contributors", "1");
-        request.query(QueryBuilders.termQuery("user", "tanguy"));
-        return client.explain(request, DEFAULT);
     }
 
     /**
