b9723079b1dcb35e121e44a28a3df28e8c005063
Thomas.XU <xuyao5@gmail.com>
2021-02-19 22:04:17 +0800

    优化ModifyByQuery

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ModifyByQuery.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ModifyByQuery.java
@@ -4,37 +4,52 @@ import io.github.xuyao5.dkl.eskits.abstr.AbstractSupporter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.index.reindex.UpdateByQueryRequest;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
 
 import javax.validation.constraints.NotNull;
+import java.util.Map;
 
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
+import static org.elasticsearch.index.reindex.AbstractBulkByScrollRequest.AUTO_SLICES;
 
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 19/02/21 21:34
- * @apiNote TODO 这里输入文件说明
- * @implNote TODO 这里输入实现说明
+ * @apiNote ModifyByQuery
+ * @implNote ModifyByQuery
  */
 @Slf4j
 public final class ModifyByQuery extends AbstractSupporter {
 
-    public ModifyByQuery(RestHighLevelClient client) {
+    private final int KEEP_ALIVE;
+
+    public ModifyByQuery(RestHighLevelClient client, int keepAlive) {
         super(client);
+        KEEP_ALIVE = keepAlive;
+    }
+
+    public ModifyByQuery(RestHighLevelClient client) {
+        this(client, 10);
     }
 
     /**
      * Update By Query API
      */
     @SneakyThrows
-    public BulkByScrollResponse updateByQuery(@NotNull QueryBuilder query, int batchSize, @NotNull String... indices) {
+    public BulkByScrollResponse updateByQuery(@NotNull QueryBuilder query, int batchSize, @NotNull String code, @NotNull Map<String, Object> params, @NotNull String... indices) {
         UpdateByQueryRequest request = new UpdateByQueryRequest(indices);
         request.setConflicts("proceed");
         request.setQuery(query);
         request.setBatchSize(batchSize);
+        request.setSlices(AUTO_SLICES);
+        request.setScroll(TimeValue.timeValueMinutes(KEEP_ALIVE));
+        request.setScript(new Script(ScriptType.INLINE, "painless", code, params));
         return client.updateByQuery(request, DEFAULT);
     }
 
@@ -43,10 +58,12 @@ public final class ModifyByQuery extends AbstractSupporter {
      */
     @SneakyThrows
     public BulkByScrollResponse deleteByQuery(@NotNull QueryBuilder query, int batchSize, @NotNull String... indices) {
-        DeleteByQueryRequest request = new DeleteByQueryRequest("source1", "source2");
+        DeleteByQueryRequest request = new DeleteByQueryRequest(indices);
         request.setConflicts("proceed");
         request.setQuery(query);
         request.setBatchSize(batchSize);
+        request.setSlices(AUTO_SLICES);
+        request.setScroll(TimeValue.timeValueMinutes(KEEP_ALIVE));
         return client.deleteByQuery(request, DEFAULT);
     }
 }
