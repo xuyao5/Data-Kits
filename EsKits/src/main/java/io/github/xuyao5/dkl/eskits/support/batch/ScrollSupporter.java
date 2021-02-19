610a0ee02cb13d4bb4d8d1b36508cbaffe1dd6fd
Thomas.XU <xuyao5@gmail.com>
2021-02-19 19:31:47 +0800

    新版本scroll

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ScrollSupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ScrollSupporter.java
@@ -3,12 +3,17 @@ package io.github.xuyao5.dkl.eskits.support.batch;
 import io.github.xuyao5.dkl.eskits.abstr.AbstractSupporter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.action.search.ClearScrollRequest;
-import org.elasticsearch.action.search.ClearScrollResponse;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchScrollRequest;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.Scroll;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
 
@@ -21,28 +26,37 @@ import static org.elasticsearch.client.RequestOptions.DEFAULT;
 @Slf4j
 public final class ScrollSupporter extends AbstractSupporter {
 
-    public ScrollSupporter(RestHighLevelClient client) {
+    private final int KEEP_ALIVE;
+
+    public ScrollSupporter(RestHighLevelClient client, int keepAlive) {
         super(client);
+        KEEP_ALIVE = keepAlive;
+    }
+
+    public ScrollSupporter(RestHighLevelClient client) {
+        this(client, 1);
     }
 
     /**
      * Search Scroll API
      */
     @SneakyThrows
-    public SearchResponse scroll() {
-        SearchScrollRequest request = new SearchScrollRequest();
-        request.scroll(TimeValue.timeValueSeconds(30));
-        return client.scroll(request, DEFAULT);
-    }
+    public ClearScrollResponse scroll(Consumer<SearchHit[]> consumer, @NotNull QueryBuilder queryBuilder, int size, @NotNull String... indices) {
+        final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(KEEP_ALIVE));
 
+        SearchResponse searchResponse = client.search(new SearchRequest(indices).source(new SearchSourceBuilder().query(queryBuilder).size(size)).scroll(scroll), DEFAULT);
+        String scrollId = searchResponse.getScrollId();
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
 
-    /**
-     * Clear Scroll API
-     */
-    @SneakyThrows
-    public ClearScrollResponse clearScroll() {
-        ClearScrollRequest request = new ClearScrollRequest();
-        request.addScrollId("scrollId");
-        return client.clearScroll(request, DEFAULT);
+        while (Objects.nonNull(searchHits) && searchHits.length > 0) {
+            consumer.accept(searchHits);
+            searchResponse = client.scroll(new SearchScrollRequest(scrollId).scroll(scroll), DEFAULT);
+            scrollId = searchResponse.getScrollId();
+            searchHits = searchResponse.getHits().getHits();
+        }
+
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scrollId);
+        return client.clearScroll(clearScrollRequest, DEFAULT);
     }
 }
