3001e7ceb1ca6d7dee85880250f9ea0742571c27
Thomas.XU <xuyao5@gmail.com>
2021-02-20 22:52:20 +0800

    补全部分代码

--- DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/configuration/DataKitsConfigBean.java
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/configuration/DataKitsConfigBean.java
@@ -2,6 +2,7 @@ package io.github.xuyao5.datakitsserver.configuration;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author Thomas.XU(xuyao)
@@ -13,4 +14,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(staticName = "of")
 public final class DataKitsConfigBean {
 
+    @Value("${es.client.hosts}")
+    private String esClientHosts;
 }
--- DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/configuration/DataKitsConfiguration.java
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/configuration/DataKitsConfiguration.java
@@ -2,9 +2,13 @@ package io.github.xuyao5.datakitsserver.configuration;
 
 import io.github.xuyao5.dkl.eskits.client.EsClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({})
+@ComponentScan(basePackages = {"io.github.xuyao5.datakitsserver"})
 public class DataKitsConfiguration {
 
     @Bean
