cc9698c1d51dc20c227891bafe481cdd94bc0bd2
Thomas.XU <xuyao5@gmail.com>
2021-02-20 23:43:31 +0800

    代码整合

--- DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/DataKitsApplication.java
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/DataKitsApplication.java
@@ -1,14 +1,31 @@
 package io.github.xuyao5.datakitsserver;
 
+import io.github.xuyao5.datakitsserver.configuration.DataKitsConfigBean;
+import io.github.xuyao5.dkl.eskits.client.EsClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "io.github.xuyao5.datakitsserver")
+@Configuration
+@Import({})
+@ComponentScan(basePackages = {"io.github.xuyao5.datakitsserver"})
 public class DataKitsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataKitsApplication.class, args);
+    }
+
+    @Bean
+    public DataKitsConfigBean DataKitsConfigBean() {
+        return DataKitsConfigBean.of();
+    }
+
+    @Bean(name = "esClient1", destroyMethod = "destroy")
+    public EsClient getEsClient() {
+        return new EsClient(new String[]{"localhost:9200"}, "", "", 10);
     }
 }
