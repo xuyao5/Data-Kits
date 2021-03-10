c238f4a5e2550cc7f21f7419bd29cde4f62c0e01
Thomas.XU <xuyao5@gmail.com>
2021-03-10 08:53:04 +0800

    修改MyDocument和test保持一致

--- DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/vo/MyDocument.java
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/vo/MyDocument.java
@@ -7,19 +7,13 @@ import lombok.ToString;
 
 import java.util.Date;
 
-/**
- * @author Thomas.XU(xuyao)
- * @implSpec 25/02/21 19:18
- * @apiNote TODO 这里输入文件说明
- * @implNote TODO 这里输入实现说明
- */
 @Data(staticConstructor = "of")
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class MyDocument extends StandardDocument {
 
     private String uuid;
-    private double amount;
-    private String chinese;
-    private Date date;
+    private double cashAmount;
+    private String desc;
+    private Date dateTime;
 }
