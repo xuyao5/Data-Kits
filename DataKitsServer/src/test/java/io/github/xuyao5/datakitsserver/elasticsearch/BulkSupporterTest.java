88d4c05b6174d71f5333b4856f3615fed299816d
Thomas.XU <xuyao5@gmail.com>
2021-03-14 22:01:49 +0800

    bug fix

--- DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/elasticsearch/BulkSupporterTest.java
+++ DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/elasticsearch/BulkSupporterTest.java
@@ -11,7 +11,7 @@ public class BulkSupporterTest extends AbstractTest {
         esClient.invokeConsumer(client -> {
             new BulkSupporter(client, esClientConfig.getEsBulkThreads()).bulk(function -> {
                 for (int i = 0; i < 1000000; i++) {
-                    function.apply(BulkSupporter.buildIndexRequest("test_index_5", String.valueOf(i), Pojo.of("xu")));
+                    function.apply(BulkSupporter.buildIndexRequest("test_index_5", Pojo.of("xu")));
                 }
             });
         });
