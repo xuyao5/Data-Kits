e763874c604ea9f0ca05f0d4f5c77e41b7864d83
Thomas.XU <xuyao5@gmail.com>
2021-03-17 22:55:05 +0800

    代码优化

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ScrollSupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ScrollSupporter.java
@@ -36,7 +36,7 @@ public final class ScrollSupporter extends AbstractSupporter {
     @SneakyThrows
     public ClearScrollResponse scroll(Consumer<SearchHit[]> consumer, @NotNull QueryBuilder queryBuilder, @NotNull String... indices) {
         final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(6));
-        SearchResponse searchResponse = client.search(new SearchRequest(indices).scroll(scroll).source(new SearchSourceBuilder().query(queryBuilder).size(RING_BUFFER_SIZE)), DEFAULT);
+        SearchResponse searchResponse = client.search(new SearchRequest(indices).scroll(scroll).source(new SearchSourceBuilder().query(queryBuilder).size(1024)), DEFAULT);
         String scrollId = searchResponse.getScrollId();
         SearchHit[] searchHits = searchResponse.getHits().getHits();
 
