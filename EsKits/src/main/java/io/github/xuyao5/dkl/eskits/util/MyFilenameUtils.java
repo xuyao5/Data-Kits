d7f98752f69384d7779181b5d356cb3e07b31e34
Thomas.XU <xuyao5@gmail.com>
2021-04-03 20:48:56 +0800

    代码整理1

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/MyFilenameUtils.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/MyFilenameUtils.java
@@ -4,8 +4,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 13/10/20 14:11
@@ -15,13 +13,4 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MyFilenameUtils extends FilenameUtils {
 
-    public static String getConfirmFilename(@NotNull String file, @NotNull String fileSeparator, @NotNull String confirmFilePrefix, @NotNull String confirmFileSuffix) {
-//        Object[] baseNameArray = Splitter.on(fileSeparator).omitEmptyStrings().trimResults().splitToList(getBaseName(file)).toArray();
-//        if (baseNameArray.length > 1) {
-//            baseNameArray[0] = confirmFilePrefix;
-//            return Joiner.on(EXTENSION_SEPARATOR).skipNulls().join(Joiner.on(fileSeparator).skipNulls().join(baseNameArray), confirmFileSuffix);
-//        }
-//        return file;
-        return null;
-    }
 }
