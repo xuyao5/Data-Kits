d1e5f1999416363ee83568a7aff15bd0d6c49e00
Thomas.XU <xuyao5@gmail.com>
2021-04-21 22:42:02 +0800

    清理代码2

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ModifyByQuerySupporter.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/ModifyByQuerySupporter.java
@@ -1,13 +1,10 @@
 package io.github.xuyao5.dkl.eskits.support.batch;
 
-import io.github.xuyao5.dkl.eskits.consts.ConflictConst;
-import io.github.xuyao5.dkl.eskits.consts.ScriptConst;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.DeleteByQueryRequest;
@@ -19,7 +16,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
-import static org.elasticsearch.index.reindex.AbstractBulkByScrollRequest.AUTO_SLICES;
+import static org.elasticsearch.index.reindex.AbstractBulkByScrollRequest.*;
 
 /**
  * @author Thomas.XU(xuyao)
@@ -39,14 +36,14 @@ public final class ModifyByQuerySupporter {
      * Update By Query API
      */
     @SneakyThrows
-    public BulkByScrollResponse updateByQuery(@NotNull RestHighLevelClient client, @NotNull QueryBuilder query, int batchSize, @NotNull String code, @NotNull Map<String, Object> params, @NotNull String... indices) {
-        UpdateByQueryRequest request = new UpdateByQueryRequest(indices);
-        request.setConflicts(ConflictConst.PROCEED.getType());
-        request.setQuery(query);
-        request.setBatchSize(batchSize);
-        request.setSlices(AUTO_SLICES);
-        request.setScroll(TimeValue.timeValueMinutes(6));
-        request.setScript(new Script(ScriptType.INLINE, ScriptConst.PAINLESS.getType(), code, params));
+    public BulkByScrollResponse updateByQuery(@NotNull RestHighLevelClient client, @NotNull QueryBuilder query, @NotNull String code, @NotNull Map<String, Object> params, @NotNull String... indices) {
+        UpdateByQueryRequest request = new UpdateByQueryRequest(indices)
+                .setQuery(query)
+                .setBatchSize(DEFAULT_SCROLL_SIZE)
+                .setSlices(AUTO_SLICES)
+                .setScroll(DEFAULT_SCROLL_TIMEOUT)
+                .setScript(new Script(ScriptType.INLINE, "painless", code, params));
+        request.setConflicts("proceed");
         return client.updateByQuery(request, DEFAULT);
     }
 
@@ -54,13 +51,13 @@ public final class ModifyByQuerySupporter {
      * Delete By Query API
      */
     @SneakyThrows
-    public BulkByScrollResponse deleteByQuery(@NotNull RestHighLevelClient client, @NotNull QueryBuilder query, int batchSize, @NotNull String... indices) {
-        DeleteByQueryRequest request = new DeleteByQueryRequest(indices);
-        request.setConflicts(ConflictConst.PROCEED.getType());
-        request.setQuery(query);
-        request.setBatchSize(batchSize);
-        request.setSlices(AUTO_SLICES);
-        request.setScroll(TimeValue.timeValueMinutes(6));
+    public BulkByScrollResponse deleteByQuery(@NotNull RestHighLevelClient client, @NotNull QueryBuilder query, @NotNull String... indices) {
+        DeleteByQueryRequest request = new DeleteByQueryRequest(indices)
+                .setQuery(query)
+                .setBatchSize(DEFAULT_SCROLL_SIZE)
+                .setSlices(AUTO_SLICES)
+                .setScroll(DEFAULT_SCROLL_TIMEOUT);
+        request.setConflicts("proceed");
         return client.deleteByQuery(request, DEFAULT);
     }
 
