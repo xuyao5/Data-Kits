45c68fd07e71ca46a83a20a77eb2e41eb4406eb5
Thomas.XU <xuyao5@gmail.com>
2021-04-23 01:35:07 +0800

    增加注释

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/configuration/ModifyByScrollConfig.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/configuration/ModifyByScrollConfig.java
@@ -6,12 +6,14 @@ import org.elasticsearch.index.query.QueryBuilder;
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 16/03/21 02:52
- * @apiNote BatchUpdateConfig
- * @implNote BatchUpdateConfig
+ * @apiNote ModifyByScrollConfig
+ * @implNote ModifyByScrollConfig
  */
 @Data(staticConstructor = "of")
 public final class ModifyByScrollConfig {
 
+    //如果sourceIndex和targetIndex相同，等于对单一索引操作
+    //如果sourceIndex和targetIndex不同，等于Merge Into操作
     private final String sourceIndex;
     private final String targetIndex;
 
