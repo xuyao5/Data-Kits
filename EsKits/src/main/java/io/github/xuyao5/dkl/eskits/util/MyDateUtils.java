e118bd102ed99c26fd68b718b89aa429769add4f
Thomas.XU <xuyao5@gmail.com>
2021-04-25 22:01:57 +0800

    update MyDateUtils

--- EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/MyDateUtils.java
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/MyDateUtils.java
@@ -2,7 +2,9 @@ package io.github.xuyao5.dkl.eskits.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 
 import javax.validation.constraints.NotNull;
@@ -28,5 +30,14 @@ public final class MyDateUtils extends DateFormatUtils {
 
     public static String getFormatDate(@NotNull FastDateFormat format) {
         return format(now(), format.getPattern(), Locale.ROOT);
+    }
+
+    @SneakyThrows
+    public static Date parseDate(@NotNull String date, @NotNull FastDateFormat format) {
+        return DateUtils.parseDate(date, format.getPattern());
+    }
+
+    public static Date parseDate(@NotNull long date) {
+        return new Date(date);
     }
 }
