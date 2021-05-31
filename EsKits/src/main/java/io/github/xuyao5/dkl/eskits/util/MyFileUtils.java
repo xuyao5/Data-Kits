5289b15b4b8fa1e5a5d27ab993ff868fbf166be2
Thomas.XU <xuyao5@gmail.com>
2021-05-31 22:42:35 +0800

    re-add MyFileUtils

---
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/MyFileUtils.java
@@ -0,0 +1,34 @@
+package io.github.xuyao5.dkl.eskits.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+
+import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 31/05/21 22:38
+ * @apiNote MyFileUtils
+ * @implNote MyFileUtils
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MyFileUtils extends FileUtils {
+
+    @SneakyThrows
+    public static List<File> getDecisionFiles(@NotNull String basePath, @NotNull String filenameRegex) {
+        try (DirectoryStream<Path> dataFileStream = Files.newDirectoryStream(Paths.get(basePath), path -> Pattern.matches(filenameRegex, path.getFileName().toString()))) {
+            return StreamSupport.stream(dataFileStream.spliterator(), false).map(Path::toFile).collect(Collectors.toList());
+        }
+    }
+}
