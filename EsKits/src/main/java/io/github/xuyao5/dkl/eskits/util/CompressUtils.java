c11ab50f6d9cecdc1f3d1dea70fba57d5c62d143
Thomas.XU <xuyao5@gmail.com>
2021-06-01 21:02:41 +0800

    增加commons-compress

---
+++ EsKits/src/main/java/io/github/xuyao5/dkl/eskits/util/CompressUtils.java
@@ -0,0 +1,15 @@
+package io.github.xuyao5.dkl.eskits.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 1/06/21 21:00
+ * @apiNote CompressUtils
+ * @implNote CompressUtils
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CompressUtils {
+
+}
