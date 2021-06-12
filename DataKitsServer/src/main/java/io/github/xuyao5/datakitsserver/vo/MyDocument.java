b9c9153a634f219df4a42bff103c043deb26c3c3
Thomas.XU <xuyao5@gmail.com>
2021-06-12 21:26:32 +0800

    delete MessageField

--- DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/vo/MyDocument.java
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/vo/MyDocument.java
@@ -1,7 +1,6 @@
 package io.github.xuyao5.datakitsserver.vo;
 
 import io.github.xuyao5.dkl.eskits.context.annotation.FileField;
-import io.github.xuyao5.dkl.eskits.context.annotation.MessageField;
 import io.github.xuyao5.dkl.eskits.schema.base.BaseDocument;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,23 +17,18 @@ public final class MyDocument extends BaseDocument {
 
     //UUID          CASH_AMOUNT          DESC          DATE_TIME_1          DATE_TIME_2
     @FileField("UUID")
-    @MessageField("UUID")
     private String uuid;
 
     @FileField("CASH_AMOUNT")
-    @MessageField("CASH_AMOUNT")
     private BigDecimal cashAmount;
 
     @FileField("DESC")
-    @MessageField("DESC")
     private String desc;
 
     @FileField("DATE_TIME_1")
-    @MessageField("DATE_TIME_1")
     private long dateTime1;
 
     @FileField("DATE_TIME_2")
-    @MessageField("DATE_TIME_2")
     private Date dateTime2;
 
     private BigDecimal discount;
