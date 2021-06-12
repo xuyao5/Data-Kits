14096941c8465a0c735b62ad4c6812b84b3b6591
Thomas.XU <xuyao5@gmail.com>
2021-06-12 16:48:01 +0800

    add rocketmq-spring-boot-starter

---
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/listener/ObjectConsumer.java
@@ -0,0 +1,17 @@
+package io.github.xuyao5.datakitsserver.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+
+@Slf4j
+@RocketMQMessageListener(topic = "${boot.rocketmq.topic}", consumerGroup = "string_consumer", selectorExpression = "${boot.rocketmq.tag}")
+public class ObjectConsumer implements RocketMQListener<Object> {
+
+    @Override
+    public void onMessage(Object message) {
+        // 重写消息处理方法
+        log.info("------- StringConsumer received:{} \n", message);
+        // TODO 对消息进行处理，比如写入数据
+    }
+}
