30c82837de87ecbd1b7c554f90f95571e733af89
Thomas.XU <xuyao5@gmail.com>
2021-06-22 22:48:37 +0800

    add HttpClientHelper

---
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/helper/HttpClientHelper.java
@@ -0,0 +1,14 @@
+package io.github.xuyao5.dkl.eskits.helper;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @implSpec 22/06/21 22:36
+ * @apiNote HttpClientHelper
+ * @implNote HttpClientHelper
+ */
+public final class HttpClientHelper {
+
+    public void invoke() {
+
+    }
+}
