9f577ee44a2ad6cebe393e01bbd8ed7d6722dde3
Thomas.XU <xuyao5@gmail.com>
2021-09-03 23:36:24 +0800

    add okhttp

--- EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/helper/HttpFsHelper.java
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/helper/HttpFsHelper.java
@@ -1,5 +1,9 @@
 package io.github.xuyao5.dkl.eskits.helper;
 
+import okhttp3.*;
+
+import java.io.IOException;
+
 /**
  * @author Thomas.XU(xuyao)
  * @implSpec 2/09/21 12:38
@@ -8,4 +12,23 @@ package io.github.xuyao5.dkl.eskits.helper;
  */
 public final class HttpFsHelper {
 
+    private final OkHttpClient httpClient;
+
+    public HttpFsHelper() {
+        httpClient = new OkHttpClient();
+    }
+
+    public void download() {
+        httpClient.newCall(new Request.Builder().url("").build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+
+            }
+        });
+    }
 }
