b3ebc847572a25a4269d8f222bb0d799411894b1
Thomas.XU <xuyao5@gmail.com>
2021-09-06 23:13:21 +0800

    更新

--- EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/helper/HttpFsHelper.java
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/helper/HttpFsHelper.java
@@ -1,5 +1,7 @@
 package io.github.xuyao5.dkl.eskits.helper;
 
+import lombok.Data;
+import lombok.NonNull;
 import okhttp3.*;
 
 import java.io.IOException;
@@ -18,8 +20,9 @@ public final class HttpFsHelper {
         httpClient = new OkHttpClient();
     }
 
-    public void download() {
-        httpClient.newCall(new Request.Builder().url("").build()).enqueue(new Callback() {
+    public FileStatus getFileStatus(@NonNull String host, int port, @NonNull String path) {
+        String url = String.format("http://%s:%d/webhdfs/v1/%s?op=GETFILESTATUS", host, port, path);
+        httpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
 
@@ -30,5 +33,60 @@ public final class HttpFsHelper {
 
             }
         });
+        return FileStatus.of();
+    }
+
+    public FileStatuses listStatus(@NonNull String host, int port, @NonNull String path) {
+        String url = String.format("http://%s:%d/webhdfs/v1/%s?op=LISTSTATUS", host, port, path);
+        httpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+
+            }
+        });
+        return FileStatuses.of();
+    }
+
+    public void openRead(@NonNull String host, int port, @NonNull String path) {
+        String url = String.format("http://%s:%d/webhdfs/v1/%s?op=OPEN", host, port, path);
+        httpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+
+            }
+        });
+    }
+
+    public void getHomeDirectory(@NonNull String host, int port) {
+        String url = String.format("http://%s:%d/webhdfs/v1/?op=GETHOMEDIRECTORY", host, port);
+        httpClient.newCall(new Request.Builder().url(url).build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+
+            }
+        });
+    }
+
+    @Data(staticConstructor = "of")
+    public static class FileStatus {
+    }
+
+    @Data(staticConstructor = "of")
+    public static class FileStatuses {
     }
 }
