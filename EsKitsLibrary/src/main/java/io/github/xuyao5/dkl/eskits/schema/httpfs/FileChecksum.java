f5cf8ac411e36445893bd527804d52d45f813f04
Thomas.XU <xuyao5@gmail.com>
2021-09-27 00:53:43 +0800

    补全

--- EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/schema/httpfs/FileChecksum.java
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/schema/httpfs/FileChecksum.java
@@ -1,5 +1,6 @@
 package io.github.xuyao5.dkl.eskits.schema.httpfs;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
@@ -11,7 +12,12 @@ import lombok.Data;
 @Data(staticConstructor = "of")
 public final class FileChecksum {
 
+    @SerializedName(value = "Algorithm", alternate = "algorithm")
     private String algorithm;
+
+    @SerializedName(value = "Bytes", alternate = "bytes")
     private String bytes;
+
+    @SerializedName(value = "Length", alternate = "length")
     private int length;
 }
