1e5dade822cb2d27f61abcb5e942a9fabe2d45cc
Thomas.XU <xuyao5@gmail.com>
2022-03-05 16:54:21 +0800

    add V2 demo

---
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/support/general/IndexSupporterV2.java
@@ -0,0 +1,37 @@
+package io.github.xuyao5.dkl.eskits.support.general;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @version 1/05/20 22:48
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class IndexSupporterV2 {
+
+    public static IndexSupporterV2 getInstance() {
+        return IndexSupporterV2.SingletonHolder.INSTANCE;
+    }
+
+    @SneakyThrows
+    public CreateIndexResponse create(@NonNull ElasticsearchClient client, @NonNull String index) {
+        return client.indices()
+                .create(createIndexBuilder -> createIndexBuilder
+                        .index(index)
+                        .aliases("foo", aliasBuilder -> aliasBuilder
+                                .isWriteIndex(true)
+                        )
+                );
+    }
+
+    private static class SingletonHolder {
+        private static final IndexSupporterV2 INSTANCE = new IndexSupporterV2();
+    }
+}
