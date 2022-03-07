e184136e277915c0bad8ba3f93f2158d8845346e
Thomas.XU <xuyao5@gmail.com>
2022-03-07 22:25:15 +0800

    demo BulkProcessor bean

--- EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/BulkSupporter.java
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/support/batch/BulkSupporter.java
@@ -107,7 +107,7 @@ public final class BulkSupporter {
             public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
                 log.error("Failed to execute bulk", failure);
             }
-        }).setConcurrentRequests(threads - 1).build()) {
+        }, "default-bulk-processor").setConcurrentRequests(threads - 1).build()) {
             consumer.accept(bulkProcessor::add);
             log.info("BulkProcessor awaitClose is {}", bulkProcessor.awaitClose(6, TimeUnit.MINUTES));
         }
