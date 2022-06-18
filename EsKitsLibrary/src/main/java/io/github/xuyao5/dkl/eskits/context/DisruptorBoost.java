1df52cc10116fa9d10361f3a88ac889a39718b90
Thomas.XU <xuyao5@gmail.com>
2022-06-18 21:15:37 +0800

    精简语句

--- EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/context/DisruptorBoost.java
+++ EsKitsLibrary/src/main/java/io/github/xuyao5/dkl/eskits/context/DisruptorBoost.java
@@ -23,24 +23,24 @@ public final class DisruptorBoost<T> {
     @Builder.Default
     private int bufferSize = 1_024 * 4;
 
-    public void processZeroArgEvent(EventFactory<T> factory, Consumer<ZeroArgEventTranslator<T>> translator, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T> handlers) {
-        processEvent(factory, ringBuffer -> translator.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
+    public void processZeroArgEvent(EventFactory<T> factory, Consumer<ZeroArgEventTranslator<T>> publisher, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T>... handlers) {
+        processEvent(factory, ringBuffer -> publisher.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
     }
 
-    public void processOneArgEvent(EventFactory<T> factory, Consumer<OneArgEventTranslator<T>> translator, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T> handlers) {
-        processEvent(factory, ringBuffer -> translator.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
+    public void processOneArgEvent(EventFactory<T> factory, Consumer<OneArgEventTranslator<T>> publisher, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T>... handlers) {
+        processEvent(factory, ringBuffer -> publisher.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
     }
 
-    public void processTwoArgEvent(EventFactory<T> factory, Consumer<TwoArgEventTranslator<T>> translator, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T> handlers) {
-        processEvent(factory, ringBuffer -> translator.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
+    public void processTwoArgEvent(EventFactory<T> factory, Consumer<TwoArgEventTranslator<T>> publisher, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T>... handlers) {
+        processEvent(factory, ringBuffer -> publisher.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
     }
 
-    public void processThreeArgEvent(EventFactory<T> factory, Consumer<ThreeArgEventTranslator<T>> translator, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T> handlers) {
-        processEvent(factory, ringBuffer -> translator.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
+    public void processThreeArgEvent(EventFactory<T> factory, Consumer<ThreeArgEventTranslator<T>> publisher, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T>... handlers) {
+        processEvent(factory, ringBuffer -> publisher.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
     }
 
-    public void processVarargEvent(EventFactory<T> factory, Consumer<VarargEventTranslator<T>> translator, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T> handlers) {
-        processEvent(factory, ringBuffer -> translator.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
+    public void processVarargEvent(EventFactory<T> factory, Consumer<VarargEventTranslator<T>> publisher, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, EventHandler<T>... handlers) {
+        processEvent(factory, ringBuffer -> publisher.accept(ringBuffer::publishEvent), exceptionHandler, isShutdownFinally, new AggregateEventHandler<>(handlers));
     }
 
     private void processEvent(EventFactory<T> factory, Consumer<RingBuffer<T>> eventProducer, ObjLongConsumer<T> exceptionHandler, boolean isShutdownFinally, AggregateEventHandler<T> eventHandler) {
@@ -64,8 +64,7 @@ public final class DisruptorBoost<T> {
             }
         });
         try {
-            disruptor.start();
-            eventProducer.accept(disruptor.getRingBuffer());
+            eventProducer.accept(disruptor.start());
         } finally {
             if (isShutdownFinally) {
                 disruptor.shutdown();
