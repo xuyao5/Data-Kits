6a2c707d0cba2a30aecbbe6b1cb6cd5cd1a4e1a9
Thomas.XU <xuyao5@gmail.com>
2022-07-14 23:10:04 +0800

    add singleInsertTest

--- DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/mybatis/MyBatisTest.java
+++ DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/mybatis/MyBatisTest.java
@@ -15,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -47,5 +50,32 @@ public class MyBatisTest extends AbstractTest {
                         log.info("批处理：{}", list.size());
                     }
                 });
+    }
+
+    @Test
+    void singleInsertTest() {
+        ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+        Date toDate = DateUtilsPlus.parse2Date("2022-12-31 23:59:59", DateUtilsPlus.STD_DATETIME_FORMAT);
+        Date fromDate = DateUtilsPlus.parse2Date("2022-01-01 00:00:00", DateUtilsPlus.STD_DATETIME_FORMAT);
+        DisruptorBoost.<OmsOrder1>context().create().processZeroArgEvent(OmsOrder1::new,
+                //事件生产
+                translator -> sourceMapper.streamQuery(fromDate, toDate, resultContext -> translator.translate((order, sequence) -> BeanUtils.copyProperties(resultContext.getResultObject(), order))),
+                //错误处理
+                (order, value) -> log.error("异常:{}|{}", value, order),
+                //事件消费
+                (omsOrder1, sequence, endOfBatch) -> {
+                    OmsOrder2 omsOrder2 = new OmsOrder2();
+                    BeanUtils.copyProperties(omsOrder1, omsOrder2);
+                    service.submit(() -> targetMapper.insertSelective(omsOrder2));
+                });
+
+        try {
+            service.shutdown();
+            boolean isClosed = service.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            log.info("线程池关闭={}", isClosed);
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+            log.error("线程池关闭失败", e);
+        }
     }
 }
