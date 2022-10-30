4337d7f27f1fadba7e88b6da09b2566a7a899651
Thomas.XU <xuyao5@gmail.com>
2022-10-30 21:25:11 +0800

    add net configuration

---
+++ DataKitsServer/src/main/java/io/github/xuyao5/datakitsserver/configuration/net/CustomClientHttpRequestInterceptor.java
@@ -0,0 +1,23 @@
+package io.github.xuyao5.datakitsserver.configuration.net;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+@Slf4j
+public class CustomClientHttpRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] bytes, ClientHttpRequestExecution execution) throws IOException {
+        // log the http request
+        log.info("URI: {}", request.getURI());
+        log.info("HTTP Method: {}", request.getMethodValue());
+        log.info("HTTP Headers: {}", request.getHeaders());
+
+        return execution.execute(request, bytes);
+    }
+}
