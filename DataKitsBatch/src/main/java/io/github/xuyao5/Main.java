06b8259b29dc3b209e51013a42d5053b919e54a7
Thomas.XU <xuyao5@gmail.com>
2022-12-15 18:38:40 +0800

    add Data Kits Batch

---
+++ DataKitsBatch/src/main/java/io/github/xuyao5/Main.java
@@ -0,0 +1,11 @@
+package io.github.xuyao5;
+
+/**
+ * @author Thomas.XU($ { USER })
+ * @version ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
