e854e18d1b0c53ada43b25ffc921fc19db755504
Thomas.XU <xuyao5@gmail.com>
2023-01-29 16:56:37 +0800

    update pom

---
+++ DataKitsBatch/src/test/java/io/github/xuyao5/datakitsbatch/job/DemoJobTest.java
@@ -0,0 +1,13 @@
+package io.github.xuyao5.datakitsbatch.job;
+
+import io.github.xuyao5.datakitsbatch.context.AbstractTest;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Thomas.XU(xuyao)
+ * @version 29/01/23 17:10
+ */
+@Slf4j
+public class DemoJobTest extends AbstractTest {
+
+}
