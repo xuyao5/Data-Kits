4631943a994cacda964e8e1151ca07ca6a9985c0
Thomas.XU <xuyao5@gmail.com>
2023-06-29 08:46:57 +0800

    add CacheLineEffect

--- DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/system/DisruptorExample.java
+++ DataKitsServer/src/test/java/io/github/xuyao5/datakitsserver/system/DisruptorExample.java
@@ -8,19 +8,16 @@ import com.lmax.disruptor.util.DaemonThreadFactory;
 public class DisruptorExample {
 
     public static void main(String[] args) throws InterruptedException {
+        //使用disruptor
         int bufferSize = 1024;
         Disruptor<Event> disruptor = new Disruptor<>(Event::new, bufferSize, DaemonThreadFactory.INSTANCE);
-
         EventHandler<Event> eventHandler = (event, sequence, endOfBatch) -> {
             // 在这里处理事件
             Thread.sleep(2);
         };
-
         disruptor.handleEventsWith(eventHandler);
         RingBuffer<Event> ringBuffer = disruptor.start();
-
         long startTime = System.currentTimeMillis();
-
         for (int i = 0; i < bufferSize; i++) {
             long sequence = ringBuffer.next();
             Event event = ringBuffer.get(sequence);
@@ -28,29 +25,24 @@ public class DisruptorExample {
             Thread.sleep(2);
             ringBuffer.publish(sequence);
         }
-
         disruptor.shutdown();
         long endTime = System.currentTimeMillis();
         System.out.println("Disruptor执行时间: " + (endTime - startTime) + "毫秒");
 
-
+        //不使用disruptor
         Event[] events = new Event[bufferSize];
-
         startTime = System.currentTimeMillis();
-
         for (int i = 0; i < bufferSize; i++) {
             Event event = new Event();
             // 设置事件数据
             Thread.sleep(2);
             events[i] = event;
         }
-
         for (int i = 0; i < bufferSize; i++) {
             Event event = events[i];
             // 在这里处理事件
             Thread.sleep(2);
         }
-
         endTime = System.currentTimeMillis();
         System.out.println("非Disruptor执行时间: " + (endTime - startTime) + "毫秒");
     }
